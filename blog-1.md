34cef991e5d5b19e7f427ef1a23163be5b563b6c
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-05-07 17:51:02 +0600

    added last 2 problem add

---
+++ blog-1.md
@@ -0,0 +1,4 @@
+Why any is a type safety hole and why unknown is safer
+🟢 Introduction
+
+TypeScript এর মূল শক্তি হলো type safety। কিন্তু any type ব্যবহার করলে এই safety নষ্ট হয়ে যায়। এজন্য any কে বলা হয় “type safety hole”। অন্যদিকে unknown বেশি safe এবং modern TypeScript এ recommended।
--- solutions.ts
+++ solutions.ts
@@ -38,3 +38,33 @@ const user = {
   age: 21,
 };
     getProperty(user, "name");
+ 
+
+//------------problem 5:------------//
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+
+function toggleReadStatus(book: Book): Book & { isRead: boolean } {
+  return {
+    ...book,
+    isRead: true,
+  };
+}
+
+const myBook = {
+  title: "TypeScript Guide",
+  author: "Jane Doe",
+  publishedYear: 2024,
+};
+ toggleReadStatus(myBook);
+
+// --------------problem 6:-----------//
+ function getIntersection(arr1: number[], arr2: number[]): number[] {
+  return [...new Set(arr1.filter((number) => arr2.includes(number)))];
+}
+
+getIntersection([1, 2,3,3, 4, 5], [3, 4, 5, 6, 7]);
