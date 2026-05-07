7986ebda2de95924ce11c54beb2b6a13f3208fab
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-05-07 19:29:15 +0600

    added somes bugs

--- solutions.ts
+++ solutions.ts
@@ -91,8 +91,9 @@ student.getDetails();
 
 
 // --------------problem 7:-----------//
+
  function getIntersection(arr1: number[], arr2: number[]): number[] {
   return [...new Set(arr1.filter((number) => arr2.includes(number)))];
 }
 
-getIntersection([1, 2,3,3, 4, 5], [3, 4, 5, 6, 7]);
+getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
