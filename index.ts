f713af75d9fd9fd27329d06f8335a5e73de7c2e2
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-05-07 16:12:54 +0600

    added problem 1

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
 //hello 
+
+// Sample Input:
+// filterEvenNumbers([1, 2, 3, 4, 5, 6])
+
+// Sample Output:
+// [2, 4, 6]
+
+
---
+++ solutions.ts
@@ -0,0 +1,5 @@
+//------------ Problem 1:-----------//
+function filterEvenNumbers(numbers: number[]): number[] {
+  return numbers.filter((number) => number % 2 === 0);
+}
+ filterEvenNumbers([1, 2, 3, 4, 5, 6, 5, 2222]);
